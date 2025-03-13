d17297fc98789b9eb4b844ea6855ba637af06cb4
Alberto <dany.canaviri2022@gmail.com>
2025-03-12 23:47:16 -0400

    Mantenimiento del Módulo Docentes finalizado

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -97,13 +97,79 @@ class DocenteController extends Controller
         return view('admin.docentes.edit', compact('docente', 'roles'));
     }
 
-    public function update(Request $request, Docente $docente)
+    public function update(Request $request, $id)
     {
-        
+
+        $docente = Docente::find($id);
+
+        $request->validate([
+            'nombres' => 'required',
+            'apellidos' => 'required',
+            'ci' => 'required|unique:docentes,ci,' . $id,
+            'fecha_nacimiento' => 'required',
+            'telefono' => 'required',
+            'direccion' => 'required',
+            'nombres' => 'required',
+            'profesion' => 'required',
+            'rol' => 'required',
+            'email' => 'required|unique:users,email,' . $docente->usuario->id,
+            'foto' => 'nullable|image'
+        ]);
+
+        $usuario = User::find($docente->usuario_id);
+        $usuario->name = $request->nombres . " " . $request->apellidos;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->ci);
+        $usuario->save();
+
+        $usuario->syncRoles($request->rol);
+
+        $docente->usuario_id = $usuario->id;
+        $docente->nombres = $request->nombres;
+        $docente->apellidos = $request->apellidos;
+        $docente->ci = $request->ci;
+        $docente->fecha_nacimiento = $request->fecha_nacimiento;
+        $docente->telefono = $request->telefono;
+        $docente->direccion = $request->direccion;
+        $docente->profesion = $request->profesion;
+
+        if ($request->hasfile('foto')) {
+
+            if (file_exists(public_path($docente->foto))) {
+                unlink(public_path($docente->foto));
+            }
+
+            $foto = $request->file("foto");
+            $nombreArchivo = time() . "_" . $foto->getClientOriginalName();
+            $rutaDestino = public_path("uploads/fotos_docente");
+            $foto->move($rutaDestino, $nombreArchivo);
+            $fotoPath = "uploads/fotos_docente/" . $nombreArchivo;
+            $docente->foto = $fotoPath;
+        }
+
+        $docente->save();
+
+        return redirect()->route("admin.docente.index")
+            ->with("mensaje", "Docente actualizado correctamente")
+            ->with("icono", "success");
     }
 
-    public function destroy(Docente $docente)
+    public function destroy($id)
     {
-        
+        $docente = Docente::find($id);
+
+        $usuario = User::find($docente->usuario_id);
+
+        if ($docente->foto && file_exists(public_path($docente->foto))) {
+            unlink(public_path($docente->foto));
+        }
+
+        $docente->delete();
+
+        $usuario->delete();
+
+        return redirect()->route("admin.docente.index")
+            ->with("mensaje", "Docente eliminado correctamente")
+            ->with("icono", "success");
     }
 }
